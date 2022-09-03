9a529693c2379c61f87bd775237893b3517b151a
marcelaproencaeng <104868735+marcelaproencaeng@users.noreply.github.com>
2022-09-02 21:08:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-serafd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-serafd)
---
+++ index.js
@@ -0,0 +1,9 @@
+const condicao = false;
+
+console.log(condicao ? 'mathias' : null);
+
+if (condicao) {
+  console.log('mathias');
+} else {
+  console.log(null);
+}
